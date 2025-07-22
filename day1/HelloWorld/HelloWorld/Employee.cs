40993c03e930782e4c973f2ade4038d98e3e83c6
Charishmagada <charishmagada@gmail.com>
2025-07-22 21:45:56 +0530

    added example of user defined data type

---
+++ day1/HelloWorld/HelloWorld/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Employee
+    {
+        public int empno;
+        public string name;
+        public double basic;
+    }
+}
---
+++ day1/HelloWorld/HelloWorld/EmployeeStaticdata.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class EmployeeStaticdata
+    {
+        static void Main()
+        {
+            Employee employee1 = new Employee();
+            employee1.empno = 1;
+            employee1.name = "Geeta";
+            employee1.basic = 88323;
+
+            Employee employee2 = new Employee();
+            employee2.empno = 2;
+            employee2.name = "Sreeja";
+            employee2.basic = 88234;
+
+            Console.WriteLine("First Employee Data  ");
+            Console.WriteLine("Employ No  " + employee1.empno);
+            Console.WriteLine("Employ Name  " + employee1.name);
+            Console.WriteLine("Employ Basic  " + employee1.basic);
+
+            Console.WriteLine("Second Employee Data  ");
+            Console.WriteLine("Employ No  " + employee2.empno);
+            Console.WriteLine("Employ Name  " + employee2.name);
+            Console.WriteLine("Employ Basic  " + employee2.basic);
+        }
+    }
+}
+

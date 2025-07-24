3098b172bd59b0875800dcc2efc03ce3268ff557
Charishmagada <charishmagada@gmail.com>
2025-07-24 22:16:08 +0530

    added example usage of shadowing

---
+++ day3/Shadowing/Shadowing/First.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shadowing
+{
+    internal class First
+    {
+        public void Show()
+        {
+            Console.WriteLine("show for class first");
+        }
+    }
+}

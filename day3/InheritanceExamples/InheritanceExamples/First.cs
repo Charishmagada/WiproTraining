dd6bc3b1a1289815a0e80916879bce2f76155bb3
Charishmagada <charishmagada@gmail.com>
2025-07-24 22:10:43 +0530

    added usage example for base keyword(overriding)

---
+++ day3/InheritanceExamples/InheritanceExamples/First.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExamples
+{
+    internal class First
+    {
+        public void Show()
+        {
+            Console.WriteLine("Show method from class first");
+                
+        }
+    }
+}
---
+++ day3/InheritanceExamples/InheritanceExamples/Second.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExamples
+{
+    internal class Second:First
+    {
+        public void Show()
+        {
+            base.Show();
+            Console.WriteLine("show method from class second");
+            /// <summary>
+            /// derived class over writes the base class.
+            /// To avoid this and method needs to be printed from both classes we make use of keyword called base
+            /// </summary>
+        }
+
+    }
+}

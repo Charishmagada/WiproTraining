6d92c48dd4ffff6c188bdc7cc18124d7124588b1
Charishmagada <charishmagada@gmail.com>
2025-08-20 19:56:43 +0530

    added jwtauthentication(react)

---
+++ day_26/jwtauth-src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/login/login';
+import Protected from './components/protected/protected';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+      <Protected/>
+    </div>
+  );
+}
+
+export default App;
